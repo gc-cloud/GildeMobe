33e112f07f2b3f00136d98aecf4ccb7ace196725
Gerardo Castaneda <gerardocastaneda@g.harvard.edu>
2016-09-18 20:07:28 -0400

    Initial commit

---
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -0,0 +1,123 @@
+package com.gildedrose;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GildedRoseTest {
+
+    @Test
+    public void testName() {
+        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("foo", app.items[0].name);
+    }
+
+    @Test
+    public void testQualityExpired() {
+        Item[] items = new Item[] { new Item("TestProduct", 0, 30) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(28 , app.items[0].quality);
+    }
+
+    @Test
+    public void testQuality() {
+        Item[] items = new Item[] { new Item("TestProduct", 10, 30) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(29 , app.items[0].quality);
+    }
+
+    @Test
+    public void testDaysReduced() {
+        Item[] items = new Item[] { new Item("TestProduct", 10, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(9 , app.items[0].sellIn);
+    }
+
+    @Test
+    public void testQualityAlwaysPositive() {
+        Item[] items = new Item[] { new Item("TestProduct", 0, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(0 , app.items[0].quality);
+    }
+    @Test
+    public void testQualityAgedBrie() {
+        Item[] items = new Item[] { new Item("Aged Brie", 1, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(11, app.items[0].quality);
+    }
+    @Test
+    public void testQualityOldAgedBrie() {
+        Item[] items = new Item[] { new Item("Aged Brie", 0, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(12, app.items[0].quality);
+        assertEquals(-1, app.items[0].sellIn);
+    }
+
+    @Test
+    public void testQualitySulfuras() {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 1, 80) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(80, app.items[0].quality);
+        assertEquals(1, app.items[0].sellIn);
+    }
+
+    @Test
+    public void testBrieQualityMoreThanFifty() {
+        Item[] items = new Item[] { new Item("Aged Brie", 1, 50) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(50, app.items[0].quality);
+        assertEquals(0, app.items[0].sellIn);
+    }
+
+    @Test
+    public void testBackstageQuality10Plus() {
+        Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 11, 20)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(21, app.items[0].quality);
+    }
+
+    @Test
+    public void testBackstageQualityUnder10() {
+        Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 9, 20)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(22, app.items[0].quality);
+    }
+
+    @Test
+    public void testBackstageQualityUnder5() {
+        Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 4, 20)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(23, app.items[0].quality);
+    }
+
+
+    @Test
+    public void testBackstageQualityExpired() {
+        Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 0, 20)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(0, app.items[0].quality);
+    }
+
+    @Test
+    public void testQualityMoreThanFifty() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 2, 49) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(50, app.items[0].quality);
+    }
+
+}
